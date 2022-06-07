fe2833b36edcb9f957fa630cd1383fc26a1c94f6
PallaviKatari <pallavikatari@gmail.com>
2022-06-07 16:29:05 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Component
+} from 'react';
+class App extends React.Component {
+      state = {
+          isLoading: true,
+          users: [],
+          error: null
+      };
+      getFetchUsers() {
+          this.setState({
+              loading: true
+          }, () => {
+              fetch("http://localhost:3000/posts").then(res => res.json()).then(result => this.setState({
+                  loading: false,
+                  users: result
+              })).catch(console.log);
+          });
+      }
+      componentDidMount() {
+          this.getFetchUsers();
+      }
+      render() {
+          const {
+              users,
+              error
+          } = this.state;
+          return (
+            <React.Fragment>
+            <h1>All User</h1>
+            {
+                  error ? <p>
+            {
+                      error.message
+                  } </p> : null}  {
+                      users.map(user => {
+                          const {
+                              adderss,
+                              name,
+                              email
+                          } = user;
+                          return (
+                          <div key={name}>
+                              <p>Name: {name}</p>
+                              <p>Email: {email}</p>
+                              <p>Address: {adderss}</p>
+                              <hr />
+                          </div>
+                          );
+                      })
+                  } </React.Fragment> );
+                 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+                  }
 export default App;
